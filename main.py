8f31892a2803604a3c1c81aa850307e663e00403
yulsep <yulimarcela99@gmail.com>
2023-03-15 00:43:22 -0500

    cors changes

--- main.py
+++ main.py
@@ -8,20 +8,25 @@ app = Flask(__name__)
 CORS(app, origins='*')
 
 
-@app.route('/course_price', methods=['POST'])
+@app.route('/check-prices', methods=['POST'])
 @cross_origin()
 def get_course_price():
     url = request.json['url']
-    r = requests.get(url)
-    html = r.text
-    doc = BeautifulSoup(html, 'html.parser')
-    price_element = doc.select_one('[data-purpose="course-price-text"]')
-    if price_element:
-        return jsonify({'url': url, 'price': price_element.text.strip()})
-    else:
-        return jsonify({'url': url, 'price': 'No price information found.'})
+    if not url.startswith('http'):
+        return jsonify({'error': 'Invalid URL'}), 400
+    try:
+        r = requests.get(url)
+        r.raise_for_status()
+        html = r.text
+        doc = BeautifulSoup(html, 'html.parser')
+        price_element = doc.select_one('[data-purpose="course-price-text"]')
+        if price_element:
+            return jsonify({'url': url, 'price': price_element.text.strip()})
+        else:
+            return jsonify({'url': url, 'price': 'No price information found in the url.'})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 
 if __name__ == '__main__':
     app.run()
-
